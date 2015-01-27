1bfef0b4420c3f7fbc3e81fb3de27feea3746501
Nat Zimmermann <nat@natzim.com>
2015-01-27 23:05:27 +0000

    Added basic forms

--- resources/views/forms/create.blade.php
+++ resources/views/forms/create.blade.php
@@ -2,4 +2,20 @@
 
 @section('content')
     <h2>Create form</h2>
+
+    {!! Form::open(['route' => 'form.store']) !!}
+
+        <div class="form-group">
+            {!! Form::label('title', 'Title') !!}
+            {!! Form::text('title', null, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('description', 'Description') !!}
+            {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
+        </div>
+
+        {!! Form::submit('Create form', ['class' => 'btn btn-primary']) !!}
+
+    {!! Form::close() !!}
 @stop
--- resources/views/forms/edit.blade.php
+++ resources/views/forms/edit.blade.php
@@ -2,4 +2,20 @@
 
 @section('content')
     <h2>Edit form</h2>
+
+    {!! Form::open(['route' => 'form.store']) !!}
+
+    <div class="form-group">
+        {!! Form::label('title', 'Title') !!}
+        {!! Form::text('title', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('description', 'Description') !!}
+        {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
+    </div>
+
+    {!! Form::submit('Update form', ['class' => 'btn btn-primary']) !!}
+
+    {!! Form::close() !!}
 @stop
