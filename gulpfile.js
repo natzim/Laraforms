0fe38b3a67d6f1fea17ca75dbcc63e18f516b756
Nat Zimmermann <nat@natzim.com>
2015-01-27 22:05:45 +0000

    Added gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,14 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
+var assetsDir = 'resources/assets';
 
 elixir(function (mix) {
-    mix.less('app.less');
+    mix.scripts([
+        'jquery/dist/jquery.min.js',
+        'bootstrap/dist/js/bootstrap.min.js'
+    ], assetsDir, 'public/js/app.min.js');
+
+    mix.styles([
+        'bootstrap/dist/css/bootstrap.min.css'
+    ], assetsDir, 'public/css/style.min.css');
 });
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,11 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laraforms</title>
 
-    <!-- Bootstrap -->
-    <link href="/bower_resources/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/style.min.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -21,9 +18,6 @@
 
     @yield('content')
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="/bower_resources/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="/js/app.min.js"></script>
 </body>
 </html>
