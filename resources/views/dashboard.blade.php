47254fe39174b4f65ad84a578ea83ba07360e3da
Nat Zimmermann <nat@natzim.com>
2015-01-28 14:03:34 +0000

    Updated dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
     <h2>Dashboard</h2>
+
+    <a role="button" class="btn btn-primary"><i class="fa fa-plus"></i> Create a new form</a>
+
+    <h3>Your forms</h3>
+
+    @foreach (Auth::user()->forms as $form)
+        {{ $form->title }}
+        {{ $form->description }}
+    @endforeach
 @stop
