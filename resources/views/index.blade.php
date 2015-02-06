f636709b496a38b5f9ba24c52f2ae4aaea1081b9
Nat Zimmermann <nat@natzim.com>
2015-02-06 11:52:03 +0000

    Changed grid layout of index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,10 +12,10 @@
     </div>
 
     <div class="container-fluid">
-        <div class="col-md-6">
+        <div class="col-sm-6">
             <img src="/img/index/apple-products.png" alt="" class="img-full-width">
         </div>
-        <div class="col-md-6">
+        <div class="col-sm-6">
             <h2>Heading</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium aliquid assumenda autem corporis deserunt dignissimos dolor dolore eaque error, facere laudantium molestias natus, nobis numquam possimus quaerat quisquam quo rem sed, temporibus totam unde ut veritatis vitae voluptates voluptatum. Assumenda, commodi delectus excepturi itaque laborum quibusdam quisquam similique. Earum.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In non quos temporibus. At autem culpa debitis deserunt dicta dignissimos eligendi est et facilis fugit, harum ipsum laudantium libero magni minima modi necessitatibus quasi qui quibusdam quisquam suscipit ut vero. Architecto cumque deleniti facilis inventore mollitia officiis pariatur quaerat quam suscipit.</p>
@@ -23,13 +23,13 @@
     </div>
 
     <div class="container-fluid">
-        <div class="col-md-6">
+        <div class="col-sm-6 col-sm-push-6">
+            <img src="http://placehold.it/500x250" alt="" class="img-full-width">
+        </div>
+        <div class="col-sm-6 col-sm-pull-6">
             <h2>Heading</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium aliquid assumenda autem corporis deserunt dignissimos dolor dolore eaque error, facere laudantium molestias natus, nobis numquam possimus quaerat quisquam quo rem sed, temporibus totam unde ut veritatis vitae voluptates voluptatum. Assumenda, commodi delectus excepturi itaque laborum quibusdam quisquam similique. Earum.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam debitis delectus deserunt dolore dolorem ea error eveniet ex excepturi explicabo fuga fugiat inventore itaque magnam minus natus nesciunt nihil nostrum porro quam reprehenderit, suscipit tempore temporibus totam ullam velit veniam veritatis vitae voluptatem. Fugiat illo incidunt molestiae quae quisquam.</p>
-        </div>
-        <div class="col-md-6">
-            <img src="http://placehold.it/500x250" alt="" class="img-full-width">
         </div>
     </div>
 @stop
